d5762bdab22f19dfa2caf5413783fd643cd598ab
Pierre Fichepoil <ExtReMLapin@users.noreply.github.com>
2015-02-05 11:02:59 +0100

    Update hudpaint.lua

--- modules/hudpaint.lua
+++ modules/hudpaint.lua
@@ -32,13 +32,15 @@ end
 function love.graphics.draw_nicegraph(x, y, w, h, tbl)
 	local max = tbl.Elements[1].DataSeries.close.values[table.GetWinningKey(tbl.Elements[1].DataSeries.close.values)]
 	local min = tbl.Elements[1].DataSeries.close.values[table.GetLoosingKey(tbl.Elements[1].DataSeries.close.values)]
-	local dolla = table.CloseValue(tbl.Positions, gesposongraph(x, y, w, h))
+	local dolla = tbl.Elements[1].DataSeries.close.values[table.CloseValue(tbl.Positions, gesposongraph(x, y, w, h))]
+	local dates = tbl.Dates[table.CloseValue(tbl.Positions, gesposongraph(x, y, w, h))]
 	xpos, ypos = love.mouse.getPosition( )
 	love.graphics.draw_graph(x, y, w, h, tbl.Elements[1].DataSeries.close.values)
 	if (xpos > x and xpos < x+w ) and (ypos > y and ypos < y+h ) then
 		love.graphics.line(xpos, y, xpos, y+h)
-		love.graphics.line(x, height - (height-(h+y)) - (math.Remap(dolla, min, max, y, h+y)) + y, x+w, ypos)
+		--love.graphics.line(x,ypos, x+w, ypos)
 		love.graphics.print("Value : ".. dolla .. " " .. tbl.Elements[1].Currency, xpos +20, ypos +10)
+		love.graphics.print(dates, xpos +20, ypos +25)
 	end
 
 end

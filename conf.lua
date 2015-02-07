9b123927b88058391aec6da0021808f3f0938d7a
Lapin <contact@extrem-team.com>
2015-02-07 15:55:27 +0100

    ddfgdfgdfgd
    fgdfgd

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.version = "0.9.1"
-	t.window.title = "Random Game"
+	t.window.title = "SafeTrade"
 	t.window.width = 1280
 	t.window.height = 720
 	t.window.resizable = false

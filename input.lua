67abf1d84e975563f4ac373247ce97d0499bd532
Lapin <contact@extrem-team.com>
2015-02-23 16:48:17 +0100

    fgsedgdfgd
    fgdfgdfg

--- input.lua
+++ input.lua
@@ -1,31 +1,28 @@
 local FIRSTPRESSED = false
-
+require("modules/hook")
 
 function love.mousepressed(x, y, button)
-	loveframes.mousepressed(x, y, button)
-	if gamestate == "playing" then
-		menuOnClick(x,y, button)
-	elseif gamestate == "formation" then
-		seFormer(x,y,button)
+	if not FIRSTPRESSED then
+		hook.Call("UniquePress", nil, x, y, button)
 	end
+	FIRSTPRESSED = true
+	loveframes.mousepressed(x, y, button)
 end
 
 function love.mousereleased(x, y, button)
+	FIRSTPRESSED = false
 	loveframes.mousereleased(x, y, button)
 
 end
 
 function love.keypressed(key, unicode)
-	if not FIRSTPRESSED then
-		hook.Call("UniquePress", nil, key, unicode)
-	end
-	FIRSTPRESSED = true
+
 	hook.Call("KeyPressed", nil, key, unicode)
 	loveframes.keypressed(key, unicode)
 end
 
 function love.keyreleased(key)
-	FIRSTPRESSED = false
+
 	hook.Call("KeyReleased", nil, key )
 	loveframes.keyreleased(key)
 end
@@ -33,3 +30,15 @@ end
 function love.textinput(text) 
 	loveframes.textinput(text)
 end
+
+
+hook.Add("UniquePress", "menupoop", function(x, y, button)
+
+	if gamestate == "playing" then
+		menuOnClick(x,y, button)
+	elseif gamestate == "formation" then
+		seFormer(x,y,button)
+	end
+
+end)
+

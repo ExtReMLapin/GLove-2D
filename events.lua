bf6b413d8e58239639dd3f1a75149160dac4a4bc
Lapin <contact@extrem-team.com>
2015-03-02 14:22:16 +0100

    khjkhj
    khjkh

--- events.lua
+++ events.lua
@@ -3,9 +3,9 @@ require("modules/hook")
 Event = {}
 Event[1] = {}
 Event[1].date = string.format("%i%i", 5, 1852)
-Event[1].func = function() print("yo mate, may the 4th be with you nigga") end
+Event[1].func = function() print("yo mate, may the 4th be with you") end
 
-PassedEvents = {}
+PassedEvents = PassedEvents or {}
 
 
 hook.Add("DateChange", "EventDateChange", function()

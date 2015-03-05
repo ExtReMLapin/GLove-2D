116a5e83db9534b1005b894ae68f5463a10e8175
Screamotion <pishalk@hotmail.fr>
2015-03-05 15:09:51 +0100

    DICK

--- clientsystem.lua
+++ clientsystem.lua
@@ -2,9 +2,9 @@ require("modules/hook")
 
 
 hook.Add("DateChange", "PuBClientCalc", function()
-	if T_SEM == 1 and T_DAY == 1  then
-		nbClients = nbClients + (Popularity*4)
-		nbClients = nbClients + (nbClients*0.04)
+	if T_SEM >= 1 and T_SEM <= 4 and T_DAY == 1  then
+		nbClients = nbClients + (Popularity)
+		nbClients = nbClients + (nbClients*0.01)
 	end
 end)
 

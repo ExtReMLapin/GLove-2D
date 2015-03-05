996c26ebdffad2de2992b234c1cd313e32f1e492
Lapin <contact@extrem-team.com>
2015-03-05 16:07:50 +0100

    vvbdfhgdfgh
    dfgdxsfgdfg

--- clientsystem.lua
+++ clientsystem.lua
@@ -2,7 +2,6 @@ require("modules/hook")
 
 
 hook.Add("DateChange", "PuBClientCalc", function()
-<<<<<<< HEAD
 	if T_SEM == 1 and T_DAY == 1 or  T_SEM == 3 and T_DAY == 1 then
 		nbClients = nbClients + (Popularity*2)
 		nbClients = nbClients + (nbClients*0.02)

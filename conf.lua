b84e01ec684637462abbbb2a255f0ea65e8dda8a
Lapin <contact@extrem-team.com>
2015-03-09 09:13:53 +0100

    Quand je dis pas touche c'est pas touche.

--- conf.lua
+++ conf.lua
@@ -4,7 +4,7 @@ function love.conf(t)
 	t.window.width = 1280
 	t.window.height = 720
 	t.window.resizable = false
-	t.window.vsync = true
+	t.window.vsync = false
 	t.window.fsaa = 0
 	t.window.highdpi = false 
 end

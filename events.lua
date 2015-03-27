2d00835ab82d2d3b60d5e17edaa0a7dc51e45a00
Screamotion <pishalk@hotmail.fr>
2015-03-27 12:56:08 +0100

    ddfffezff

--- events.lua
+++ events.lua
@@ -60,7 +60,6 @@ hook.Add("DateChange", "Taxes", function()
 	if T_SEM == 1 and T_DAY == 1 then
 		addMoney(-12000, "Location locaux")
 		addMoney(-6500 * nbEmployees, "Salaires")
-		--addMoney(-6000, "Location mensuelle")
 	end
 	if T_MONTH == 1 and T_SEM == 1 and T_DAY == 1 then
 		addMoney(-annualPayment, "Dividendes")

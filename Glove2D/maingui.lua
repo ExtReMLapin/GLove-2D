75f6e39ccf0dfe6a12ddfa97ace356d30d011299
Lapin <contact@extrem-team.com>
2015-05-04 09:49:10 +0200

    fix

--- Glove2D/maingui.lua
+++ Glove2D/maingui.lua
@@ -273,10 +273,11 @@ hook.Add("SaveRestored", "gui create", function()
 	
 	MenuDown.Update = function(object, dt)
 	local MOUSE_X, MOUSE_Y = love.mouse.getPosition( )
+	if MOUSE_X > MenuDown:GetX() and MOUSE_X < (MenuDown:GetX() + MenuDown:GetWidth()) and MOUSE_Y > MenuDown:GetY() and MOUSE_Y < (MenuDown:GetY() + MenuDown:GetHeight() - 36) then  
 			if MenuDown:GetY() > ScrH - (MenuDown:GetHeight()-50) then
 			MenuDown:SetPos(MenuDown:GetX() ,  math.Approach( MenuDown:GetY(), ScrH - (MenuDown:GetHeight()-50), -1*math.Remap(50,0,love.timer.getFPS( ),0,60 )))
 			end
-		else
+	else
 			if MenuDown:GetY() == ScrH-25 then return end
 			if MenuDown:GetY() < ScrH-25 then MenuDown:SetPos(MenuDown:GetX(),math.Approach( MenuDown:GetY(), ScrH - 25, math.Remap(50,0,love.timer.getFPS( ),0,60 ) )) end
 			if MenuDown:GetY() > ScrH-25 then MenuDown:SetPos(MenuDown:GetX(),math.Approach( MenuDown:GetY(), ScrH - 25, -1*math.Remap(50,0,love.timer.getFPS( ),0,60 ) )) end

d329a58695bec7045be7890392ae3e9b6d7d1df9
Lapin <contact@extrem-team.com>
2015-05-09 11:07:16 +0200

    EMPLOYEES + fond

--- Glove2D/background.lua
+++ Glove2D/background.lua
@@ -3,6 +3,7 @@ ScrH_RATIO = 1
 
 local blurrywall = love.graphics.newImage("ressources/wallpoop.png")
 local backgroundpic = love.graphics.newImage("ressources/BackgroundMedium.png")
+local backgroundpic2 = love.graphics.newImage("ressources/BackgroundOver.png")
 local secretaryPic = love.graphics.newImage("ressources/CharaSecretaryAnim1.png")
 local comptoirPic = love.graphics.newImage("ressources/ObjectCounter.png")
 --local casierPic = love.graphics.newImage("ressources/casier.png")
@@ -18,7 +19,17 @@ local customers = {
 	love.graphics.newImage("ressources/CharaCustomer3Anim1.png")
 }
 
---[[
+local emps = {
+	
+	love.graphics.newImage("ressources/CharaSalarymen1Stand.png"),
+	love.graphics.newImage("ressources/CharaSalarymen2Stand.png"),
+	love.graphics.newImage("ressources/CharaSalarymen3Stand.png"),
+	love.graphics.newImage("ressources/CharaSalarymen4Stand.png"),
+	love.graphics.newImage("ressources/CharaSalarymen5Stand.png")
+}
+
+
+
 
 local VAR = 0
 
@@ -35,7 +46,7 @@ slider2:SetSlideType("vertical")
 slider2.OnValueChanged = function(object)
     VAR = object:GetValue()
     print(VAR)
-end]]
+end
 
 
 
@@ -76,6 +87,17 @@ local quad = love.graphics.newQuad(0, 0, blurrywall:getWidth( ), blurrywall:getH
 	local quad = love.graphics.newQuad(0,0 ,x*210/1280,y*162/720,x*210/1280,y*162/720)
 	love.graphics.draw(secretaryPic,quad ,midx - x*150/1280, midy - y*200/720)
 
+
+
+	for k, v in pairs(emps) do
+		local xxx = x*50/1280
+		local yyy = y*75/720
+		local quad = love.graphics.newQuad(0,0, xxx,yyy,xxx,yyy)
+		love.graphics.draw(v,quad ,
+		midx + x*(-170+k*75-k*k*3.5)/1280,
+		midy + y*(-12-k*31)/720)
+	end
+
 	local quad = love.graphics.newQuad(0,0, x*425/1280,y*305/720,x*425/1280,y*305/720)
 	love.graphics.draw(comptoirPic,quad ,midx - x*180/1280, midy - y*183/720)
 
@@ -89,12 +111,10 @@ local quad = love.graphics.newQuad(0, 0, blurrywall:getWidth( ), blurrywall:getH
 
 	for k, v in pairs(customers) do
 
-		local quad = love.graphics.newQuad(0,0, v:getWidth( ), v:getHeight( ),x*93/1280,y*103/720)
+		local quad = love.graphics.newQuad(0,0,x*93/1280,y*103/720,x*93/1280,y*103/720)
 		love.graphics.draw(v,quad ,
 		midx + x*(-151+k*75-k*k*3.5)/1280,
 		midy + y*(29-k*31)/720)
-
-
 	end
 
 	local quad = love.graphics.newQuad(0,0, bossPic:getWidth( ), bossPic:getHeight( ),x*111.5/1280,y*139.375/720)
@@ -105,6 +125,11 @@ local quad = love.graphics.newQuad(0, 0, blurrywall:getWidth( ), blurrywall:getH
 
 	local quad = love.graphics.newQuad(0,0, secu3Pic:getWidth( ), secu3Pic:getHeight( ),x*111.5/1280,y*145/720)
 	love.graphics.draw(secu3Pic,quad ,midx - x*(350-135)/1280, midy- y*(120+69)/720)
+
+
+
+		local quad = love.graphics.newQuad(0,0, backgroundpic:getWidth( )-1, backgroundpic:getHeight( ),x,y)
+	love.graphics.draw(backgroundpic2,quad ,midx - x/2, midy - y/2)
 end)
 
 

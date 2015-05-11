d9f32c9f643ba8a119e47c20e4a40617d08fbbc7
Lapin <contact@extrem-team.com>
2015-05-11 11:00:08 +0200

    dddddddd
    ddddddddddddddddddddddddd

--- Glove2D/background.lua
+++ Glove2D/background.lua
@@ -66,8 +66,14 @@ local function remplace_custommer(id)
 end
 
 
+timer.create("clientsdraw", 6, 0, function()
+	local num = math.random(5)
+	remplace_custommer(num)
+end)
+
+
 local VAR = 0
---[[
+
 local frame = loveframes.Create("frame")
 frame:SetName("Slider")
 frame:SetSize(300, 275)
@@ -83,7 +89,7 @@ end
 
 
 
-
+--[[
 local slider2 = loveframes.Create("slider", frame)
 slider2:SetPos(5, 60)
 slider2:SetHeight(200)

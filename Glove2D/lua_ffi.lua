720db4584827fe575539b5bc6f9b24585fdd9e3e
Pierre <pfichepo@e3r11p11.42.fr>
2015-06-16 22:09:01 +0200

    OS fix + Released game

--- Glove2D/lua_ffi.lua
+++ Glove2D/lua_ffi.lua
@@ -1,15 +1,1 @@
 if not jit.os == "Windows" then return end
-
-
-ffi.cdef "int SDL_SetHint(const char *name, const char *value);"
-
-ffi.cdef[[
-		void Sleep(int ms);
-		int poll(struct pollfd *fds, unsigned long nfds, int timeout);
-	]]
-
-ffi.load( 'User32.dll' )
-
-
-
-
